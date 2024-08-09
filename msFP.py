2c230a3290bed6d9bf54c6002245a00d77a59380
Halil İbrahim Aykıroğlu <halil.i.aykiroglu@gmail.com>
2024-08-09 17:11:41 +0300

    Item Type Gruplaması yapabiliyor.

--- msFP.py
+++ msFP.py
@@ -3,29 +3,41 @@
 from msLogger import logger
 from msConfig import maStConf
 from pprint import pp
-byLocation={}
-byType={}
 
-with open(maStConf.fileToProcess,"r",encoding="utf-8") as file:
-    content=file.readlines()
+byLocation = {}
+allTypes = {
+    "BOOK": ["ARTBOOK", "BOOK", "EASYBOOK", "MANUSCRIPT", "PUBTHESIS", "RES_ARTBO", "RES_BOOK", "RES_REFBK", "WEEKLYBOOK"]
+    # Diğer kategoriler burada eklenebilir
+}
 
-for info in content:
-    cleared=info.lstrip().rstrip("|\n").replace(" ","|")
-    information=cleared.split("|")
-    location=information[2]
-    if location not in byLocation.keys():
-        byLocation[location]=int(information[0])
-    else:
-        byLocation[location]+=int(information[0])
+with open(maStConf.fileToProcess, "r", encoding="utf-8") as file:
+    content = file.readlines()
 
 for info in content:
-    cleared=info.lstrip().rstrip("|\n").replace(" ","|")
-    information=cleared.split("|")
-    itemType=information[1]
-    if itemType not in byType.keys():
-        byType[itemType]=int(information[0])
-    else:
-        byLocation[location]+=int(information[0])
-byLocation["ALLSPECIALCOL"]=byLocation["H-ALIYUCEL"]+byLocation['INALCIK']+byLocation["SPECIALCOL"]
+    cleared = info.lstrip().rstrip("|\n").replace(" ", "|")
+    information = cleared.split("|")
+    count = int(information[0])
+    itemType = information[1]
+    location = information[2]
+    
+    # itemType'ın hangi kategoriye ait olduğunu bulma
+    found_category = None
+    for category, types in allTypes.items():
+        if itemType in types:
+            found_category = category
+            break
+    
+    if found_category is None:
+        # Eğer itemType herhangi bir kategoriye ait değilse, kendisi bir kategori olur
+        found_category = itemType
+    
+    # Sözlük yapısını oluşturma ve toplama
+    if location not in byLocation:
+        byLocation[location] = {}
+    
+    if found_category not in byLocation[location]:
+        byLocation[location][found_category] = 0
+    
+    byLocation[location][found_category] += count
 
-pp(byType)
+pp(byLocation)
